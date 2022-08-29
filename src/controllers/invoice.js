208a0a91c1a145b723b8c374c622154a6bf94a9d
Thiago Moreira <thiago.moreira@luizalabs.com>
2022-08-29 09:39:14 -0300

    add endpoint count

--- src/controllers/invoice.js
+++ src/controllers/invoice.js
@@ -2,6 +2,7 @@ const Model = require('../models/invoice')
 
 module.exports = {
     getAll,
+    count,
     getByProtocol,
     insert
 }
@@ -11,11 +12,15 @@ async function getAll(request, response) {
     return response.json({ invoices })
 }
 
+async function count(request, response) {
+    const invoices = await Model.findAll();
+    return response.json({ count: invoices.length })
+}
+
 async function getByProtocol(request, response) {
     const invoice = await Model.findOne({ where: { protocol: request.params.protocol } });
     return response.json({ invoice })
 }
-
 
 async function insert(request, response) {
     const invoice = request.body;
--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -2,6 +2,7 @@ const controller = require('../controllers/invoice')
 
 module.exports = (app) => {
     app.get('/invoice', controller.getAll)
+    app.get('/invoice-count', controller.count)
     app.get('/invoice/:protocol', controller.getByProtocol)
     app.post('/invoice/callback', controller.insert)
 }
